6f4030aeb536fb4c42ee008eda0322dd81bc74f8
linhproht <vnguyenduylinh@gmail.com>
2023-03-01 17:26:55 +0700

    Add Commit button

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -23,6 +24,8 @@ class UserType extends AbstractType
                 'first_options' => ['label' => 'Password'],
                 'second_options' => ['label' => 'Confirm Password']
             ])
+            ->add('save', SubmitType::class,[
+                'label' => 'Confirm'])
         ;
     }
 
